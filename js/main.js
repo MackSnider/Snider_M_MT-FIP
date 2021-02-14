ccc5bbbd6998d810fc886fc9ec42748e66282e4d
Mackenzie Snider <MackSnider@users.noreply.github.com>
2021-02-14 14:48:01 -0500

    finished midterm

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 console.log("javacript is now linked up");
 
+	function myFunction1() {
+		var text1="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui sint perferendis architecto consequatur suscipit laudantium minus ratione quae dolore officia. Voluptates iste eum, voluptas minima nostrum ratione eos maxime ipsum!"
+
+	document.getElementById("para1").innerHTML
+	= text1;
+		}
+
+	function myFunction2() {
+		var text1="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui sint perferendis architecto consequatur suscipit laudantium minus ratione quae dolore officia. Voluptates iste eum, voluptas minima nostrum ratione eos maxime ipsum!"
+		
+	document.getElementById("para2").innerHTML
+	= text1;
+		}
+
+	function myFunction3() {
+		var text1="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui sint perferendis architecto consequatur suscipit laudantium minus ratione quae dolore officia. Voluptates iste eum, voluptas minima nostrum ratione eos maxime ipsum!"
+		
+	document.getElementById("para3").innerHTML
+	= text1;
+		}
+
+		function learnMore() {
+		var text1="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui sint perferendis architecto consequatur suscipit laudantium minus ratione quae dolore officia. Voluptates iste eum, voluptas minima nostrum ratione eos maxime ipsum!"
+		
+	document.getElementById("learnMoreText").innerHTML
+	= text1;
+		}
